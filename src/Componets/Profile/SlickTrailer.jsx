0b82bc9d7d01bd2f6b40c79b08d7bf58dc6f9a1b
CapTaindev <computermaytinh232@gmail.com>
2023-01-15 15:43:12 +0700

    fix video trailer

--- src/Componets/Profile/SlickTrailer.jsx
+++ src/Componets/Profile/SlickTrailer.jsx
@@ -9,13 +9,16 @@ import Box from '@mui/material/Box';
 import Modal from '@mui/material/Modal';
 import PlayArrowIcon from '@mui/icons-material/PlayArrow';
 import tmdbConfigs from 'api/Config/tmdb.config';
+import YouTube from 'react-youtube';
+import { Typography } from '@mui/material';
 
 function SlickTrailer({ Videos, title }) {
    const [open, setOpen] = useState(false);
+   const [videoId, setvideoId] = useState('');
    const swiperRef = useRef(null);
 
-   const handleOpen = () => setOpen(true);
    const handleClose = () => setOpen(false);
+   console.log(videoId);
 
    return (
       <div className='relative  w-full '>
@@ -30,6 +33,29 @@ function SlickTrailer({ Videos, title }) {
                <KeyboardArrowRightIcon fontSize='large' />
             </button>
          </div>
+         <Modal
+            open={open}
+            keepMounted
+            onClose={handleClose}
+            aria-labelledby='keep-mounted-modal-title'
+            aria-describedby='keep-mounted-modal-description'
+         >
+            <Box onClick={handleClose} className='absolute my-2 flex h-screen w-full  items-center justify-center '>
+               <YouTube
+                  videoId={videoId}
+                  opts={{
+                     height: '540',
+                     width: '960',
+
+                     playerVars: {
+                        // https://developers.google.com/youtube/player_parameters
+                        autoplay: 1,
+                     },
+                  }}
+               />
+            </Box>
+         </Modal>
+
          <div className='mt-1 w-full'>
             <Swiper
                slidesPerView={1}
@@ -52,7 +78,14 @@ function SlickTrailer({ Videos, title }) {
             >
                {Videos.map((video, index) => {
                   return (
-                     <SwiperSlide onClick={handleOpen} key={index} className='overflow-hidden '>
+                     <SwiperSlide
+                        onClick={() => {
+                           setvideoId(video.key);
+                           setOpen(true);
+                        }}
+                        key={index}
+                        className='overflow-hidden '
+                     >
                         <img
                            src={tmdbConfigs.youtubeThumnail(video.key, 'maxresdefault')}
                            alt='trailer'
@@ -65,34 +98,6 @@ function SlickTrailer({ Videos, title }) {
                         <p className='absolute  left-2 top-2 rounded-full bg-secondary/50 px-2  text-center text-[12px] leading-4  text-Orange '>
                            {video.type}
                         </p>
-                        {/* <Modal
-                           keepMounted
-                           open={open}
-                           onClose={handleClose}
-                           aria-labelledby='keep-mounted-modal-title'
-                           aria-describedby='keep-mounted-modal-description'
-                        >
-                           <Box
-                              sx={{
-                                 position: 'absolute',
-                                 top: '50%',
-                                 left: '50%',
-                                 transform: 'translate(-50%, -50%)',
-                                 width: 400,
-                                 bgcolor: 'background.paper',
-                                 border: '2px solid #000',
-                                 boxShadow: 24,
-                                 p: 4,
-                              }}
-                           >
-                              <div id='keep-mounted-modal-title' variant='h6' component='h2'>
-                                 Text in a modal
-                              </div>
-                              <div id='keep-mounted-modal-description' sx={{ mt: 2 }}>
-                                 Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-                              </div>
-                           </Box>
-                        </Modal> */}
                      </SwiperSlide>
                   );
                })}
